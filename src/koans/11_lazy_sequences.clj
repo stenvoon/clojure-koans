b1630ddefcdc5083ffed2439d9e493554af1f53e
Steve Neely <steve@unsupervised.com>
2019-12-12 15:25:37 -0700

    fixed 11

--- src/koans/11_lazy_sequences.clj
+++ src/koans/11_lazy_sequences.clj
@@ -17,12 +17,12 @@
      (drop 95 (range 100)))
 
   "Iteration provides an infinite lazy sequence"
-  (= __ (take 8 (iterate (fn [x] (* x 2)) 1)))
+  (= [1 2 4 8 16 32 64 128] (take 8 (iterate (fn [x] (* x 2)) 1)))
 
   "Repetition is key"
   (= [:a :a :a :a :a :a :a :a :a :a]
-     (repeat 10 __))
+     (repeat 10 :a))
 
   "Iteration can be used for repetition"
   (= (repeat 100 "hello")
-     (take 100 (iterate ___ "hello"))))
+     (take 100 (iterate (fn [x] (conj x)) "hello"))))
