7521458186db20bc0ad0aea098a15d65f8dc756e
Steve Neely <steve@unsupervised.com>
2019-12-12 11:00:20 -0700

    Filling back in solutions for 01_equalities and 02_strings from latest koans

--- src/koans/01_equalities.clj
+++ src/koans/01_equalities.clj
@@ -24,16 +24,16 @@
   (= true (not (= 1 nil)))
 
   "Strings, and keywords, and symbols: oh my!"
-  (= __ (= "hello" :hello 'hello))
+  (= false (= "hello" :hello 'hello))
 
   "Make a keyword with your keyboard"
-  (= :hello (keyword __))
+  (= :hello (keyword "hello"))
 
   "Symbolism is all around us"
-  (= 'hello (symbol __))
+  (= 'hello (symbol "hello"))
 
   "What could be equivalent to nothing?"
-  (= __ nil)
+  (= (last "") nil)
 
   "When things cannot be equal, they must be different"
   (not= :fill-in-the-blank "fill-in-the-blank"))
