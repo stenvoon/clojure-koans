19d9b7f15c753c68dc8cef2f2044466681ce8b55
Steve Neely <steve.neely@ocelotbot.com>
2020-08-30 21:27:48 -0600

    21-23 done

--- src/koans/21_partition.clj
+++ src/koans/21_partition.clj
@@ -9,7 +9,7 @@
   (= '((:a :b :c)) (partition 3 [:a :b :c :d :e]))
 
   "You can use partition-all to include any leftovers too"
-  (= __ (partition-all 3 (range 5)))
+  (= '((0 1 2) (3 4)) (partition-all 3 (range 5)))
 
   "If you need to, you can start each sequence with an offset"
   (= '((0 1 2) (5 6 7) (10 11 12)) (partition 3 5 (range 13)))
--- src/koans/22_group_by.clj
+++ src/koans/22_group_by.clj
@@ -15,7 +15,7 @@
      [[1 3 5] [2 4]])
 
   "You can also group by a primary key"
-  (= {1 [{:name "Bob", :id 1} {:last-name "Smith", :id 1}], 2 [{:name "Mike", :id 2}]}
+  (= {1 [{:name "Bob", :id 1} {:last-name "Smith", :id 1}], 2 [{:name "Jennifer", :id 2}]}
      (group-by :id [{:id 1 :name "Bob"}
                     {:id 2 :name "Jennifer"}
                     {:id 1 :last-name "Smith"} ]))
@@ -23,14 +23,14 @@
   "But be careful when you group by a non-required key"
   (= {"Bob" [{:name "Bob" :id 1}]
       "Jennifer" [{:name "Jennifer" :id 2}]
-      __ [{:last-name "Smith" :id 1}]}
+      nil [{:last-name "Smith" :id 1}]}
    (group-by :name [{:id 1 :name "Bob"}
                     {:id 2 :name "Jennifer"}
                     {:id 1 :last-name "Smith"}]))
 
   "The true power of group-by comes with custom functions"
   (= {:naughty-list [{:name "Jimmy" :bad true} {:name "Joe" :bad true}]
-      :nice-list [{:name "Jack" :bad false}]}
+      :nice-list [{:name "Jane" :bad false}]}
      (group-by #(if (:bad %) :naughty-list :nice-list)
                [{:name "Jimmy" :bad true}
                 {:name "Jane" :bad false}
